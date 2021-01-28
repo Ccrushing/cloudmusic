98efc4e21ade8b527f0fe678deabebd98393e953
Crush <1562980903@qq.com>
2021-01-28 19:31:58 +0800

    歌曲列表高亮&跳转播放页

--- miniprogram/components/musiclist/musiclist.js
+++ miniprogram/components/musiclist/musiclist.js
@@ -10,10 +10,23 @@ Component({
   /**
    * 组件的初始数据
    */
-  data: {},
+  data: {
+    playingId : -1
+  },
 
   /**
    * 组件的方法列表
    */
-  methods: {}
+  methods: {
+    onSelect(event){
+    const ds = event.currentTarget.dataset
+    console.log(ds)
+    this.setData({
+      playingId:ds.musicid
+    })
+    wx.navigateTo({
+      url: `../../pages/player/player?musicId=${ds.musicid}&index=${ds.index}`,
+    })
+    }
+  }
 })
