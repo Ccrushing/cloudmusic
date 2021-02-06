fe9348ef12223901d4f7afd38d55d92f750b92f1
Crush <1562980903@qq.com>
2021-02-06 18:46:17 +0800

    自定义授权登录和弹框组件

--- miniprogram/pages/find/find.js
+++ miniprogram/pages/find/find.js
@@ -7,8 +7,42 @@ Page({
   },
   onLoad: function (options) {},
   onPublish() {
+    
+    //获取用户的当前设置，返回值中只会出现小程序已经向用户请求过的权限，
+    //根据是否具有scope.userInfo属性，判断用户是否授权
+    wx.getSetting({
+      success:(res) => {
+        console.log('当前设置' + JSON.stringify(res))
+        if(res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success:(res) => {
+              console.log(res)
+              this.onLoginSuccess({
+                detail: res.userInfo
+              })
+            }
+          })
+        }else{
+          this.setData({
+            modalShow:true,
+          })
+        }
+      }
+    })
+  },
+  onLoginSuccess(event){
+    console.log('>>>>>>' + event)
+    const detail = event.detail
+    console.log(detail.nickName)
     wx.navigateTo({
       url: '../publish/publish',
+    })
+
+  },
+  onLoginFail(){
+    wx.showModal({
+      title:'授权用户才能发布',
+      content:'',
     })
   },
 
